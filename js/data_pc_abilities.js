1978a9acd52d5100ebf5866f75a897fda7cf052b
GreenManSK <lukas.kurcik@gmail.com>
2023-07-14 21:13:51 +0200

    Add player abilities

---
+++ js/data_pc_abilities.js
@@ -0,0 +1,13 @@
+data_pc_abilities = [
+  {
+    title: "Sneak Attack",
+    icon: "backstab",
+    subtitle: "1 per turn",
+    description: "Onec per turn if using finesse or ranged weapon",
+    reference: "",
+    bullets: [
+      "If you have advanteg on the attack roll.",
+      "Another enemy of the target is within 5ft of enemy and you do not have disadvantage.",
+    ],
+  },
+];
